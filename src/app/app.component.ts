cb2ec9e30a6667468cfa0ab9e2072f6489cfc365
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2022-06-30 00:20:59 +0300

    added children routes and menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vitoff';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,25 @@ import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Routes, RouterModule } from "@angular/router";
 import { BlockRepairComponent } from './block-repair/block-repair.component';
+import { GrusAutoComponent } from './grus-auto/grus-auto.component';
+import { TrailerEquipmentComponent } from './trailer-equipment/trailer-equipment.component';
+import { LightVehicleComponent } from './light-vehicle/light-vehicle.component';
+
+
+const routesChildren: Routes = [
+  { path: 'grus-auto', component: GrusAutoComponent },
+  { path: 'trailer', component: TrailerEquipmentComponent },
+  { path: 'vehicle-car', component: LightVehicleComponent },
+  { path: '**', component: NotFoundComponent }
+]
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'repair', component: BlockRepairComponent },
-  { path: '**', component: NotFoundComponent }
-]
+  { path: 'repair', component: BlockRepairComponent, children: routesChildren },
+  { path: "**", component: NotFoundComponent}
+  ]
 
 @NgModule({
   declarations: [
@@ -22,7 +33,10 @@ const routes: Routes = [
     HomeComponent,
     AboutComponent,
     NotFoundComponent,
-    BlockRepairComponent
+    BlockRepairComponent,
+    GrusAutoComponent,
+    TrailerEquipmentComponent,
+    LightVehicleComponent
   ],
   imports: [
     BrowserModule,
